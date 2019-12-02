e7e9d8675e0cfccad0c05e655eb65cdb9d91f53f
web-padawan <iamkulykov@gmail.com>
2019-12-02 14:50:38 +0200

    2.0.0-alpha1

--- src/vaadin-accordion.ts
+++ src/vaadin-accordion.ts
@@ -35,7 +35,7 @@ export class VaadinAccordion extends KeyboardDirectionMixin(SlottedItemsMixin(Va
   static is = 'vaadin-accordion';
 
   static get version() {
-    return '1.0.1';
+    return '2.0.0-alpha1';
   }
 
   /**
