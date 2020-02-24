5637705cef3750d75d6e4fccf5e8835eaddee8f9
web-padawan <iamkulykov@gmail.com>
2020-02-24 10:15:24 +0200

    fix: register theme styles with module id

--- theme/material/vaadin-accordion-panel-styles.ts
+++ theme/material/vaadin-accordion-panel-styles.ts
@@ -1,4 +1,4 @@
 import { registerStyles } from '@vaadin/vaadin-themable-mixin/register-styles.js';
 import { accordionPanelStyles } from './vaadin-accordion-panel-css';
 
-registerStyles('vaadin-accordion-panel', accordionPanelStyles);
+registerStyles('vaadin-accordion-panel', accordionPanelStyles, { moduleId: 'material-accordion-panel' });
